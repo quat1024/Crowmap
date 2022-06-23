397763616ab8ad8aeb84098668e40b3e9c17ad8b
quat1024 <quat1024@users.noreply.github.com>
2022-06-23 01:59:44 -0400

    Dont display tooltip messages if localizations arent loaded
    Forge versions of the mod had a config option to make the
    tooltip disappear, but I'm not adding that on Fabric if i have
    to bring my own configuration library lol

--- src/main/java/agency/highlysuspect/crowmap/mixin/client/FilledMapItemHoverTextMixin.java
+++ src/main/java/agency/highlysuspect/crowmap/mixin/client/FilledMapItemHoverTextMixin.java
@@ -2,6 +2,8 @@ package agency.highlysuspect.crowmap.mixin.client;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MapItem;
@@ -22,6 +24,11 @@ public class FilledMapItemHoverTextMixin {
 		at = @At("TAIL")
 	)
 	public void appendHoverTextAfter(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced, CallbackInfo ci) {
+		//Don't display the tooltip if there's no mod installed that loads resourcepacks out of jars.
+		//I mean, the mod still works (there's no need to hard-dep on fabric-resource-loader or anything),
+		//but the tooltip will look ugly, so just don't do it. Hardcoding English strings could be another option but idc.
+		if(!Language.getInstance().has("crowmap.tooltip.hello")) return;
+		
 		if(Screen.hasShiftDown()) {
 			tooltipComponents.add(Component.translatable("crowmap.tooltip.hello").withStyle(ChatFormatting.DARK_AQUA));
 			tooltipComponents.add(Component.translatable("crowmap.tooltip.info").withStyle(ChatFormatting.DARK_AQUA));
