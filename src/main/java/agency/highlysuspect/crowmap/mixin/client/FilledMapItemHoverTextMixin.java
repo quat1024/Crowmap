db5988699a82e8639f3a01e65801d6f3a0cc9c42
quat1024 <quat1024@users.noreply.github.com>
2024-10-16 20:23:07 -0400

    Tooltip now self-destructs after you read it

--- src/main/java/agency/highlysuspect/crowmap/mixin/client/FilledMapItemHoverTextMixin.java
+++ src/main/java/agency/highlysuspect/crowmap/mixin/client/FilledMapItemHoverTextMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,21 +19,113 @@ import java.util.List;
 
 @Mixin(MapItem.class)
 public class FilledMapItemHoverTextMixin {
+	
+	@Unique
+	int tooltipState = -1;
+	
+	@Unique long byeMillis;
+	
+	@Unique private static final Component HOLD_SHIFT =
+		Component.translatable("crowmap.tooltip.hide").withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC);
+	@Unique private static final Component INFO1 =
+		Component.translatable("crowmap.tooltip.hello").withStyle(ChatFormatting.DARK_AQUA);
+	@Unique private static final Component INFO2 =
+		Component.translatable("crowmap.tooltip.info").withStyle(ChatFormatting.DARK_AQUA);
+	@Unique private static final Component BYE =
+		Component.translatable("crowmap.tooltip.bye").withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC);
+	
+	@Unique private void addHoldShiftPrompt(List<Component> tooltipComponents) {
+		tooltipComponents.add(HOLD_SHIFT);
+	}
+	
+	@Unique private void addInfo(List<Component> tooltipComponents) {
+		tooltipComponents.add(INFO1);
+		tooltipComponents.add(INFO2);
+	}
+	
+	@Unique private void addBye(List<Component> tooltipComponents) {
+		tooltipComponents.add(BYE);
+	}
+	
+	@SuppressWarnings("UnnecessaryContinue")
 	@Inject(
 		method = "appendHoverText",
 		at = @At("TAIL")
 	)
 	public void appendHoverTextAfter(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced, CallbackInfo ci) {
-		//Don't display the tooltip if there's no mod installed that loads resourcepacks out of jars.
-		//I mean, the mod still works (there's no need to hard-dep on fabric-resource-loader or anything),
-		//but the tooltip will look ugly, so just don't do it. Hardcoding English strings could be another option but idc.
-		if(!Language.getInstance().has("crowmap.tooltip.hello")) return;
+		if(level == null) return;
 		
-		if(Screen.hasShiftDown()) {
-			tooltipComponents.add(Component.translatable("crowmap.tooltip.hello").withStyle(ChatFormatting.DARK_AQUA));
-			tooltipComponents.add(Component.translatable("crowmap.tooltip.info").withStyle(ChatFormatting.DARK_AQUA));
-		} else {
-			tooltipComponents.add(Component.translatable("crowmap.tooltip.hide").withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC));
+		boolean shifting = Screen.hasShiftDown();
+		
+		//it's a state machine! "continue" transitions states.
+		
+		for(int i = 0; i <= 3; i++) {
+			switch(tooltipState) {
+				//state -1: haven't checked to see if languages work yet.
+				//goes to state 0 if they do, and -2 if they don't. (-2 is a dead end)
+				case -1 -> {
+					if(!Language.getInstance().has("crowmap.tooltip.hello")) {
+						tooltipState = -2;
+						return;
+					} else {
+						tooltipState = 0;
+						continue;
+					}
+				}
+				
+				//state 0: haven't pressed shift yet.
+				//displays <Hold Shift> and goes to state 1 when they do.
+				case 0 -> {
+					if(!shifting) {
+						addHoldShiftPrompt(tooltipComponents);
+						return;
+					} else {
+						tooltipState = 1;
+						continue;
+					}
+				}
+				
+				//state 1: holding shift, looking at the message.
+				//displays the message and goes to state 2 when the release shift.
+				case 1 -> {
+					if(shifting) {
+						addInfo(tooltipComponents);
+						return;
+					} else {
+						tooltipState = 2;
+						byeMillis = System.currentTimeMillis();
+						continue;
+					}
+				}
+				
+				//state 2: displaying the "this tooltip will now hide itself" message.
+				//transitions back to state 1 when shift is pressed, or to state 3 after 5 seconds.
+				case 2 -> {
+					if(shifting) {
+						//back to state 1
+						tooltipState = 1;
+						continue;
+					} else if(System.currentTimeMillis() - byeMillis <= 5000) {
+						addBye(tooltipComponents);
+						return;
+					} else {
+						tooltipState = 3;
+						continue;
+					}
+				}
+				
+				//state 3: no "hold shift" prompt. silently display message on shift only.
+				case 3 -> {
+					if(shifting) {
+						addInfo(tooltipComponents);
+					}
+					return;
+				}
+				
+				default -> {
+					return;
+				}
+			}
 		}
 	}
 }
