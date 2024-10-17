282b28c980dbb73c34c37ef8fdd9e670a1c0ead7
quat1024 <quat1024@users.noreply.github.com>
2024-10-16 21:31:20 -0400

    1.21.1

--- src/main/java/agency/highlysuspect/crowmap/mixin/client/FilledMapItemHoverTextMixin.java
+++ src/main/java/agency/highlysuspect/crowmap/mixin/client/FilledMapItemHoverTextMixin.java
@@ -8,8 +8,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MapItem;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
